5caf4c15fbff1a95bd191476d83ffd2dbc10b15c
alicat219028310 <alicatgoat@gmail.com>
2026-08-28 06:38:20 -0700

    Update operations-on-file.py

--- operations-on-file.py
+++ operations-on-file.py
@@ -34,7 +34,7 @@ file = open("class-notes.txt", "r")
 lines = file.readlines()
 file.close()
 out = open("odd-lines.txt", "w")
-for i in range(0, len*(lines), 2):
+for i in range(0, len(lines), 2):
     out.write(lines[i])
 out.close()
 print("odd lines saved to odd-lines.txt")
